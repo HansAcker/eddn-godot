8f662d98904a2d8bc79989cbd20708ea9c34179c
Hans Acker <>
2023-04-15 17:08:27 +0200

    parenthal cleanup

--- ws/class_star_system_record.gd
+++ ws/class_star_system_record.gd
@@ -41,7 +41,7 @@ static func parse_star_class(from: Dictionary) -> StringName:
 
 static func parse_starpos(from: Dictionary) -> Vector3:
 	var starpos = from.get("StarPos")
-	if !((starpos is Array && len(starpos) == 3) &&
+	if !(starpos is Array && len(starpos) == 3 &&
 			(starpos[0] is float) && (starpos[1] is float) && (starpos[2] is float)):
 		return POS_INVALID
 	return Vector3(starpos[0], starpos[1], starpos[2])
