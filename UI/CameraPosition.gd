61ecc8a8262b128b8687d373b3884fd62d24cdc5
Hans Acker <>
2023-04-16 16:22:28 +0200

    show camera position in ED coordinates

--- UI/CameraPosition.gd
+++ UI/CameraPosition.gd
@@ -3,4 +3,4 @@ extends Label
 
 func _process(delta: float) -> void:
 	var camera_position := get_viewport().get_camera_3d().position
-	text = "%.2f, %.2f, %2.f" % [camera_position.x, camera_position.y, camera_position.z]
+	text = "%.2f, %.2f, %2.f" % [-camera_position.x, camera_position.y, camera_position.z]  ## Adjust camera x to ED galaxy
