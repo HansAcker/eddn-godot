37ec2a38361fa2cc57ccc843baef620ad95f1476
Hans Acker <>
2023-04-16 14:52:52 +0200

    added camera position label

---
+++ UI/CameraPosition.gd
@@ -0,0 +1,6 @@
+extends Label
+
+
+func _process(delta: float) -> void:
+	var camera_position := get_viewport().get_camera_3d().position
+	text = "%.2f, %.2f, %2.f" % [camera_position.x, camera_position.y, camera_position.z]
