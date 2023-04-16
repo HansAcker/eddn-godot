ab730d2128ec957659b051f62223f63f8526fbd5
Hans Acker <>
2023-04-16 23:09:40 +0200

    event x was inverted

--- UI/UI_EDDNEvent.gd
+++ UI/UI_EDDNEvent.gd
@@ -4,7 +4,7 @@ func _on_eddn_receiver_received(event_type: StringName, message: Dictionary, sta
 	$Type.text = event_type
 	$SystemName.text = star_system.name
 	#$Position.text = "%s" % star_system.position if star_system.position != StarSystemRecord.POS_INVALID else ""
-	$Position.text = "x: %10.3fly y: %10.3fly z: %10.3fly" % [-star_system.position.x, star_system.position.y, star_system.position.z] if star_system.position != StarSystemRecord.POS_INVALID else ""
+	$Position.text = "x: %10.3fly y: %10.3fly z: %10.3fly" % [star_system.position.x, star_system.position.y, star_system.position.z] if star_system.position != StarSystemRecord.POS_INVALID else ""
 	#$Position.text = "[table][tr][th]x[/th][th]y[/th][th]z[/th][/tr][tr][td]%9.3f[/td][td]%9.3f[/td][td]%9.3f[/td][/tr][/table]" % [star_system.position.x, star_system.position.y, star_system.position.z] if star_system.position != StarSystemRecord.POS_INVALID else ""
 	$Timestamp.text = "%s (%ss ago)" % [message.timestamp, age]
 
