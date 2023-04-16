307660265ad02220d937e9b6cd61ea5bfb85a150
Hans Acker <>
2023-04-16 23:27:22 +0200

    un-cascaded field checks

--- ws/class_star_system_record.gd
+++ ws/class_star_system_record.gd
@@ -17,13 +17,18 @@ var position: Vector3
 static func parse_system_name(from: Dictionary) -> String:
 	## TODO: StarSystem vs SystemName vs System. consult EDDN documentation.
 	var system_name = from.get("StarSystem")
-	if !(system_name is String || system_name is StringName):
-		system_name = from.get("SystemName")
-		if !(system_name is String || system_name is StringName):
-			system_name = from.get("System")
-			if !(system_name is String || system_name is StringName):
-				return ""
-	return system_name as String
+	if (system_name is String || system_name is StringName):
+		return system_name as String
+
+	system_name = from.get("SystemName")
+	if (system_name is String || system_name is StringName):
+		return system_name as String
+
+	system_name = from.get("System")
+	if (system_name is String || system_name is StringName):
+		return system_name as String
+
+	return ""
 
 static func parse_system_id(from: Dictionary) -> int:
 	var system_id = from.get("SystemAddress")
@@ -34,11 +39,14 @@ static func parse_system_id(from: Dictionary) -> int:
 static func parse_star_class(from: Dictionary) -> StringName:
 	## TODO: StarClass vs StarType. consult EDDN documentation.
 	var star_class = from.get("StarType")
-	if !(star_class is String || star_class is StringName):
-		star_class = from.get("StarClass")
-		if !(star_class is String || star_class is StringName):
-			return &""
-	return StringName(star_class)
+	if (star_class is String || star_class is StringName):
+		return StringName(star_class)
+
+	star_class = from.get("StarClass")
+	if (star_class is String || star_class is StringName):
+		return StringName(star_class)
+
+	return &""
 
 static func parse_starpos(from: Dictionary) -> Vector3:
 	var starpos = from.get("StarPos")
