3e191de8578e889a3967c875993bb51a07a2d556
Hans Acker <>
2023-04-17 21:18:25 +0200

    pre-compute expire_msec, fwiw; updated alpha scale minimum to 0.3

--- ws/GenericEvent.gd
+++ ws/GenericEvent.gd
@@ -6,10 +6,15 @@ extends Node
 
 
 ## Expire time in seconds. 0 to keep forever.
-@export var expire: int = 0
+@export var expire: int = 0:
+	set(value):
+		expire = value
+		_expire_msec = value * 1000
 
 
 @onready var star_manager := get_node("../../../Map/StarManager") as StarManager
+
+var _expire_msec: int
 
 func _on_eddn_receiver_received(event_type: StringName, _message: Dictionary, star_system: StarSystemRecord, age: int) -> void:
 	if star_system.position != StarSystemRecord.POS_INVALID:
@@ -18,4 +23,4 @@ func _on_eddn_receiver_received(event_type: StringName, _message: Dictionary, st
 			print("Empty name for %s in %d %s" % [event_type, star_system.id, star_system.position])
 
 		## TODO: configurable alpha scale
-		star_manager.add(star_system, expire * 1000, 1.0 - (clampi(age, 0, 3600) / 4000.0) if age > 10 else 1.0)
+		star_manager.add(star_system, _expire_msec, 1.0 - (clampi(age, 0, 2800) / 4000.0) if age > 10 else 1.0)
--- ws/NavRoute.gd
+++ ws/NavRoute.gd
@@ -5,9 +5,15 @@ extends Node
 
 
 ## Expire time in seconds. 0 to keep forever.
-@export var expire: int = 0
+@export var expire: int = 0:
+	set(value):
+		expire = value
+		_expire_msec = value * 1000
+
 
 @onready var star_manager := get_node("../../../Map/StarManager") as StarManager
+
+var _expire_msec : int
 
 func _on_eddn_receiver_received(event_type: StringName, message: Dictionary, _star_system: StarSystemRecord, age: int) -> void:
 	if event_type != &"NavRoute":
@@ -19,7 +25,7 @@ func _on_eddn_receiver_received(event_type: StringName, message: Dictionary, _st
 		return
 
 	## TODO: configurable alpha scale
-	var alpha := 1.0 - (clampi(age, 0, 3600) / 4000.0) if age > 10 else 1.0
+	var alpha := 1.0 - (clampi(age, 0, 2800) / 4000.0) if age > 10 else 1.0
 
 #	var line : Array[Vector3] = [StarSystemRecord.parse(route[0]).position]
 
@@ -27,7 +33,7 @@ func _on_eddn_receiver_received(event_type: StringName, message: Dictionary, _st
 		var wp := StarSystemRecord.parse(_wp)
 #		line.push_back(wp.position)
 		## TODO: check for POS_INVALID?
-		star_manager.add(wp, expire * 1000,  alpha)
+		star_manager.add(wp, _expire_msec,  alpha)
 
 	## TODO: draw lines
 #	print("Line: ", line)
