deaf164abbabe4cdd0708955666a33a6507b48f6
Hans Acker <>
2023-04-20 20:49:27 +0200

    configurable precision. logging changes.

--- Stars/EDSMQuery.gd
+++ Stars/EDSMQuery.gd
@@ -12,10 +12,13 @@ extends Node
 @export var edsm_api_base : String = "https://www.edsm.net"
 #@export var edsm_api_base : String = "https://test.eddn-realtime.space/.edsmcache"
 
-## Radius of search sphere in ly
-@export var query_radius : int = 20
+## Snap center position to multiples of this value.
+@export var location_precision : float = 1.0
 
-## Minimum distance in ly
+## Radius of search sphere in ly.
+@export var radius : int = 20
+
+## Minimum distance from center in ly.
 @export var min_radius : int = 0
 
 ## Expire time in seconds. 0 to keep forever.
@@ -25,75 +28,85 @@ extends Node
 		_expire_msec = value * 1000
 
 
-@onready var http_request = HTTPRequest.new()
+var _expire_msec : int
+
+var http_request = HTTPRequest.new()
 
 @onready var star_manager := get_node("../StarManager") as StarManager
-
-var _expire_msec : int
 
 
 func _ready() -> void:
 	add_child(http_request)
-	http_request.request_completed.connect(self._http_request_completed)
+	http_request.request_completed.connect(_http_request_completed)
+
 
 func add_stars_at(position: Vector3) -> void:
-	var center := Vector3(snappedf(-position.x, query_radius / 2.0), snappedf(position.y, query_radius / 2.0), snappedf(position.z, query_radius / 2.0))
+	var center := Vector3(snappedf(-position.x, location_precision), snappedf(position.y, location_precision), snappedf(position.z, location_precision))
 	print("EDSM query center: %s" % center)
+
 	var error = http_request.request(edsm_api_base + "/api-v1/sphere-systems?x=%s&y=%s&z=%s&radius=%d&minRadius=%d&showId=1&showCoordinates=1&showPrimaryStar=1" %
-			["%.5f" % center.x, "%.5f" % center.y, "%.5f" % center.z, query_radius, min_radius])
+			["%.5f" % center.x, "%.5f" % center.y, "%.5f" % center.z, radius, min_radius])
 	if error != OK:
-		push_error("An error occurred in the HTTP request.")
+		push_error("EDSM query error in the HTTP request.")
+
 
 func _http_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	## TODO: maybe extract X-Rate-Limit-Limit, -Remaining, -Reset
+#	print(headers)
+
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var _response = json.get_data()
 	if !(_response is Array):
-		print("not an Array")
+		print("EDSM query returned no data")
 		return
 
 	var response := _response as Array
+	var count : int = 0
 
 	for _sys in response:
-		var sys_entry := StarSystemRecord.new()
-
 		if !(_sys is Dictionary):
-			print("not a Dictionary")
+			print("EDSM response: array member not a Dictionary")
 			continue
+
 		var sys := _sys as Dictionary
+		var sys_entry := StarSystemRecord.new()
 
 		var id = sys.get("id64")
 		if !(id is int || id is float):
-			print("no ID: %s" % sys)
+			print("EDSM response: no id64 in %s" % sys)
 			continue
 		sys_entry.id = id
 
 		var name = sys.get("name")
 		if !(name is String):
-			print("no name")
+			print("EDSM response: no name in %s" % sys)
 			continue
 		sys_entry.name = name
 
 		var _coords = sys.get("coords")
 		if !(_coords is Dictionary):
-			print("no coords")
+			print("EDSM response: no coords in %s" % sys)
 			continue
+
 		var coords := _coords as Dictionary
+
 		var x = coords.get("x")
 		if !(x is float):
-			print("wrong coords")
+			print("EDSM response: invalid coords in %s" % sys)
 			continue
+
 		var y = coords.get("y")
 		if !(y is float):
-			print("wrong coords")
+			print("EDSM response: invalid coords in %s" % sys)
 			continue
+
 		var z = coords.get("z")
 		if !(z is float):
-			print("wrong coords")
+			print("EDSM response: invalid coords in %s" % sys)
 			continue
-		sys_entry.position = Vector3(-x, y, z)
 
-		var count : int = 0
+		sys_entry.position = Vector3(-x, y, z)
 
 		var _primary = sys.get("primaryStar")
 		if _primary is Dictionary:
@@ -101,16 +114,16 @@ func _http_request_completed(result: int, response_code: int, headers: PackedStr
 			var _star_class = primary.get("type")
 			if _star_class is String:
 				var star_class := (_star_class as String).split(" ", false, 1)[0]
+				## TODO: comparing String vs StringName. fix mapping from EDSM names.
 				if !(star_class in StarClasses.colors):
-					print("no color for star_class %s (%s)" % [star_class, _star_class])
+					print("EDSM response: no color for star class %s (%s)" % [star_class, _star_class])
 					sys_entry.star_class = &""
 				else:
 					sys_entry.star_class = StringName(star_class)
 
 #		print("adding %s at %s" % [sys_entry.name, sys_entry.position])
 		star_manager.add(sys_entry, _expire_msec, 1.0, false)
-		
+
 		count += 1
-		if count >= 1:  ## TODO: values > 1 don't submit stars in batches as expected
-			await get_tree().physics_frame
-			count = 0
+
+	print("EDSM added %d stars" % count)
