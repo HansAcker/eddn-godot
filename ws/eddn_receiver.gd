b044daf3d50feb6728ab04a66a8cec3186551004
Hans Acker <>
2023-08-29 01:48:41 +0200

    removed old code

--- ws/eddn_receiver.gd
+++ ws/eddn_receiver.gd
@@ -57,23 +57,6 @@ func _on_json_web_socket_receiver_received(data: Dictionary) -> void:
 		var event_type := StringName(_event_type)
 
 		received.emit(event_type, message, StarSystemRecord.parse(message), age)
-
-#		var system_id = message.get("SystemAddress")
-#		var starpos = message.get("StarPos")
-#
-#		if !((system_id is int || system_id is float) &&
-#				(starpos is Array && len(starpos) == 3) &&
-#				(starpos[0] is float) && (starpos[1] is float) && (starpos[2] is float)):
-#
-#			## NavRoute, FCMaterials, ...? don't carry system location
-#			if event_type not in [&"NavRoute", &"FCMaterials"]:
-#				print("EDDN event without system address: %s" % event_type)
-#				## TODO: whitelist events and return here?
-#				#return
-#
-#			system_id = -1 ## arbitrary dummy ID. TODO: does ID int(-1) exist?
-#			starpos = [0.0, 100e3, 0.0] ## arbitrary dummy location. TODO: use [0,0,0]?
-#		received.emit(event_type, message, system_id as int, Vector3(starpos[0], starpos[1], starpos[2]))
 	else:
 		## market/shipyard/outfitting updates also end up here
 		#received.emit("Event", message)
